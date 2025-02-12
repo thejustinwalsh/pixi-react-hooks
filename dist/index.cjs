affef215526fdfb5b32e19cf8c545d04215bebcf
Justin Walsh <contact.me@thejustinwalsh.com>
2025-02-12 12:24:50 -0500

    chore: build

--- dist/index.cjs
+++ dist/index.cjs
@@ -1,4 +1,4 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _optionalChain(ops) { let lastAccessLHS = undefined; let value = ops[0]; let i = 1; while (i < ops.length) { const op = ops[i]; const fn = ops[i + 1]; i += 2; if ((op === 'optionalAccess' || op === 'optionalCall') && value == null) { return undefined; } if (op === 'access' || op === 'optionalAccess') { lastAccessLHS = value; value = fn(value); } else if (op === 'call' || op === 'optionalCall') { value = fn((...args) => value.call(lastAccessLHS, ...args)); lastAccessLHS = undefined; } } return value; }
 
 
 
@@ -6,14 +6,14 @@
 
 
 
-var _chunkY6YBMKNJcjs = require('./chunk-Y6YBMKNJ.cjs');
+var _chunkDZ2C4WEOcjs = require('./chunk-DZ2C4WEO.cjs');
 
 // src/useAssets.ts
 var _react = require('react');
 function useAssets(urls) {
-  const [state, setState, thenable] = _chunkY6YBMKNJcjs.useAssetState.call(void 0, urls, _chunkY6YBMKNJcjs.isLoaded, _chunkY6YBMKNJcjs.load, _chunkY6YBMKNJcjs.resolve);
+  const [state, setState, thenable] = _chunkDZ2C4WEOcjs.useAssetState.call(void 0, urls, _chunkDZ2C4WEOcjs.isLoaded, _chunkDZ2C4WEOcjs.load, _chunkDZ2C4WEOcjs.resolve);
   _react.useEffect.call(void 0, () => {
-    thenable == null ? void 0 : thenable.then((data) => setState({ isLoaded: true, error: null, data })).catch((error) => setState({ isLoaded: true, error, data: void 0 }));
+    _optionalChain([thenable, 'optionalAccess', _ => _.then, 'call', _2 => _2((data) => setState({ status: "loaded", isLoaded: true, error: null, data })), 'access', _3 => _3.catch, 'call', _4 => _4((error) => setState({ status: "error", isLoaded: false, error, data: null }))]);
   }, [setState, thenable]);
   return state;
 }
@@ -21,9 +21,9 @@ function useAssets(urls) {
 // src/useAssetBundle.ts
 
 function useAssetBundle(bundles) {
-  const [state, setState, thenable] = _chunkY6YBMKNJcjs.useAssetState.call(void 0, bundles, _chunkY6YBMKNJcjs.isBundleLoaded, _chunkY6YBMKNJcjs.loadBundle, _chunkY6YBMKNJcjs.resolveBundle);
+  const [state, setState, thenable] = _chunkDZ2C4WEOcjs.useAssetState.call(void 0, bundles, _chunkDZ2C4WEOcjs.isBundleLoaded, _chunkDZ2C4WEOcjs.loadBundle, _chunkDZ2C4WEOcjs.resolveBundle);
   _react.useEffect.call(void 0, () => {
-    thenable == null ? void 0 : thenable.then((data) => setState({ isLoaded: true, error: null, data })).catch((error) => setState({ isLoaded: true, error, data: void 0 }));
+    _optionalChain([thenable, 'optionalAccess', _5 => _5.then, 'call', _6 => _6((data) => setState({ status: "loaded", isLoaded: true, error: null, data })), 'access', _7 => _7.catch, 'call', _8 => _8((error) => setState({ status: "error", isLoaded: false, error, data: null }))]);
   }, [setState, thenable]);
   return state;
 }
